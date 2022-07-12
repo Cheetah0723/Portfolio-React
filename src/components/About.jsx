f57379eea7c1100f9e017c11685584dc519b15eb
Cheetah0723 <kimuraa.hideyo@gmail.com>
2022-07-12 15:58:10 -0400

    fix warning

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import { Container, Col, Row } from "react-bootstrap";
-import PropTypes from "prop-types";
-import Fade from "react-reveal";
-import Header from "./Header";
-import endpoints from "../constants/endpoints";
-import FallbackSpinner from "./FallbackSpinner";
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Container, Col, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Fade from 'react-reveal';
+import Header from './Header';
+import endpoints from '../constants/endpoints';
+import FallbackSpinner from './FallbackSpinner';
 
 const styles = {
   introTextContainer: {
