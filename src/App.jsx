59ec599890feee5d0d75d7653624a0c0a0201502
Cheetah0723 <kimuraa.hideyo@gmail.com>
2022-07-12 15:49:12 -0400

    cjange app warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import useDarkMode from "use-dark-mode";
-import AppContext from "./AppContext";
-import MainApp from "./MainApp";
-import GlobalStyles from "./theme/GlobalStyles";
-import { lightTheme, darkTheme } from "./theme/themes";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
+import AppContext from './AppContext';
+import MainApp from './MainApp';
+import GlobalStyles from './theme/GlobalStyles';
+import { lightTheme, darkTheme } from './theme/themes';
 
 function App() {
   window.matchMedia = null;
