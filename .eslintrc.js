21f1860123078ed7ead9af55a613775769bf5497
Amazing web dev <91611393+Cheetah0723@users.noreply.github.com>
2022-07-15 14:42:03 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
-    'prefer-template': 0,
-    'react/no-children-prop': [0],
-    'linebreak-style': 0
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
 };
